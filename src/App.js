2c81dcfab5320eaed1d36222eac1c5d4fb73de4f
parwatsinghpanwar <parwatpanwar8316@gmail.com>
2026-03-10 12:23:48 +0530

    Refactor App component structure and update CSS styles

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="maindiv">
+     <div id="header"> header section </div>
+     <div id="menu"> menu section </div>
+     <div id="content"> content section </div>
+     <div id="footer"> footer section </div>
     </div>
   );
 }
