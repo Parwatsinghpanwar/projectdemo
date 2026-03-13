ee57dc420d34c8a02e9d1d35400471d580733576
parwatsinghpanwar <parwatpanwar8316@gmail.com>
2026-03-13 12:12:16 +0530

    Update Home component to display a welcome message and the sum of two numbers

--- src/components/homecomponent/home.js
+++ src/components/homecomponent/home.js
@@ -1,9 +1,12 @@
 import './home.css'
 
 function Home(){
+    var a=12,b=5,c;
+    c=a+b
     return(
   <div id= "home">
-    Home page called 
+    <h1>this is a homepage of website</h1>
+    <p>sum={c}</p>
   </div>  
  
     );
