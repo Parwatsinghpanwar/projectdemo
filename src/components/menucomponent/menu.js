85a241a183fb3494baee2764d95829444e40b16e
parwatsinghpanwar <parwatpanwar8316@gmail.com>
2026-03-13 12:36:08 +0530

    Fix case sensitivity in menu links and refactor Service component for state management

--- src/components/menucomponent/menu.js
+++ src/components/menucomponent/menu.js
@@ -5,7 +5,7 @@ function menu(){
         <div id="menu">
             <Link to="/"> home</Link>
             <Link to="/About">about</Link>
-            <Link to="/service">service</Link>
+            <Link to="/Service">service</Link>
             <Link to="/login">Login</Link>
             r<Link to="/registor">registor</Link>
             </div>
--- src/components/servicescomponent/service.js
+++ src/components/servicescomponent/service.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './service.css'
 
-function service(){
+function Service(){
+    const[count,setcount]=useState(0)
+    function increment()
+    {
+        setcount(count+1)
+    }
+    function decrement()
+    {
+        setcount(count-1)
+    }
     return(
   <div id= "service">
-    service page called 
+   <h1> service page called</h1> 
+    <spam>count-{count}</spam>
+    <button onClick={increment}>+</button>
+    <br/>
+    <button onClick={decrement}>-</button>
   </div>  
  
     );
 }
-export default service;
+export default Service;
